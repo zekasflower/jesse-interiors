b3420f81ed416e321a7e476cf992a15babc8cf7b
Abdullah <abdallaramdan2021@gmail.com>
2022-08-24 14:35:55 +0200

    About Me Section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import WebisteNav from '@/components/WebisteNav.vue'
 import HeroSection from '@/components/HeroSection.vue'
 import BrandsSection from '@/components/BrandsSection.vue'
+import AboutMeSection from '@/components/AboutMeSection.vue'
 </script>
 
 <template>
   <WebisteNav />
   <HeroSection />
   <BrandsSection />
+  <AboutMeSection />
 </template>
---
+++ src/components/AboutMeSection.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="about-me">
+    <div class="container">
+      <div class="content-wrapper">
+        <div class="left-section">
+          <img src="@/assets/images/about/main.png" class="main-img">
+          <img src="@/assets/images/about/alt.png" class="alt-img">
+        </div>
+        <div class="right-section">
+          <h2 class="main-title has-circle !mb-14">About Me</h2>
+          <h4>"Everything is designed. Few things are designed well."</h4>
+          <p>A pellentesque pretium feugiat vel morbi sagittis lorem habitasse cursus. Suspendisse tempus rhoncus enim pellentesque est in neque, elit mattis vehicula vitae eget. Viverra imperdiet felis sollicitudin tesque cursus sit.</p>
+          <p>Feugiat hac porttitor sem dictum velit, elementum odio. Sed ipsum quis nec ut eget facilisis. Quis nibh bibendum nisl quis placerat proin tortor. Mattis in neque, elit mattis morbi sagittis lorem habitasse consequat.</p>
+          <button class="btn-primary mt-14">
+            Read more <Icon icon="heroicons-outline:arrow-narrow-right" class="icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="postcss" scoped>
+.about-me {
+  @apply pt-60 pb-52;
+  .content-wrapper {
+    @apply flex gap-x-32;
+    .left-section {
+      @apply flex-1 relative;
+      .main-img { @apply ml-auto }
+      .alt-img { @apply absolute -top-[90px] left-5 border-[14px] border-neutral-50 }
+    }
+    .right-section {
+      @apply flex-1;
+      h4 { @apply text-[26px] mb-5 }
+      p { @apply text-lg font-light mb-7 leading-loose }
+    }
+  }
+}
+</style>
